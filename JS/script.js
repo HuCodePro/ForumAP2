68fc35323b7dccea7c04f9cccc1f2e072e9bba44
Hugo <hugodcps.pro@gmail.com>
2024-11-27 10:46:17 +0100

    perm denied

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,7 @@
-Document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
     console.log("Le script est chargé");
     const buttonSignup = document.getElementById("buttonSignup") 
     const dialog = document.getElementById("dialog"); 
-
-    buttonSignup.addEventListener("click", () => {
-        alert("Le bouton fonctionne !");
-      });
-      
   
     buttonSignup.addEventListener("click", () => {
       dialog.showModal();
