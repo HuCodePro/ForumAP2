9df4b151b5b69e455fd4038d112d973c07215c34
root <root@HTTPS-4663>
2024-11-27 08:29:18 +0100

    init

---
+++ JS/Components/signupCompo.js
@@ -0,0 +1,13 @@
+Document.addEventListener("DOMontentLoaded", () => {
+    const buttonSignup = document.getElementById("buttonSignup")
+    const popupContainer = document.getElementById("popupContainer")
+
+    function closePopup () {
+        popupContainer.innerHTML = ''
+    }
+
+    function openPopup () {
+        
+    }
+    
+})
